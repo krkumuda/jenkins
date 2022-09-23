791bd9f45315438e5e44efb7a68546396eb682c0
krkum <krkum@KUMUDA>
2022-09-23 13:22:41 +0530

    second commit

---
+++ src/test/java/Practice/Practice4.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice4 {
+
+	@Test
+	public void Practic4() {
+		Reporter.log("practice4",true);
+}
+}
